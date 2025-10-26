60e31f6afa27f1d42b1b5fcbf102ab5b8b65b885
uraurasan <uraurasan0720@gmail.com>
2025-10-27 02:17:11 +0900

    feat: お問い合わせフォームの全機能実装とDB設定完了

---
+++ src/app/Http/Controllers/ContactController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ContactRequest;
+use App\Models\Contact;
+
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+      return view('index');
+      }
+
+    public function confirm(ContactRequest $request)
+    {
+        $contact = $request->only(['name', 'email', 'tel', 'content']);
+        return view('confirm', compact('contact'));
+      }
+
+    public function store(ContactRequest $request)
+    {//ここから回答を確認
+      $contact = $request->only(['name', 'email', 'tel', 'content']);
+      //ここまで回答を確認
+      Contact::create($contact);
+      return view('thanks');
+      }
+}
